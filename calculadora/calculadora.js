1b32aa35d328a94ff46178b982e041e87274e4b9
suellensanches <suellen.ssanches4@gmail.com>
2026-03-11 11:19:53 -0300

    Aplicativo calculadora

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -1,34 +1,64 @@
-function calculadora(){
+
+function calculadora() {
+    
     while(true) {
-    let resultado = 0.0;
-    let operando1;
-    let operando2;
-    let operador;
+        let resultado = 0.0;
+        let operando1;
+        let operando2;
+        let operador;
+        let continua;
+        let operador_valido = true;
 
-    operando1 = parseFloat( prompt("Digite o primeiro número: "));
-    operando2 = parseFloat( prompt("Digite o segundo número: "));
-    operador = prompt("Digite uma das operações (+ - * / ): ");
+        operando1 = parseFloat(prompt("Digite o primeiro número: "));
+        operador = prompt("Digite uma das operações ( + - * / ): ");
+        operando2 = parseFloat(prompt("Digite o segundo número: "));
+        
 
-    if ( operador === "+") {
-        resultado = operando1 + operando2;
-    } else if ( operador === "-"){
-        resultado = operando1 - operando2;
-    } else if ( operador === "*" ){
-        resultado = operando1 * operando2;
-    } else if ( operador === "/" ){
-        if (operando2 == 0) {
-         alert("Não é possível dividir por zero")
-        return
+        if ( operador === "+" ) {
+            resultado = operando1 + operando2;
+        } else if (operador === "-") {
+            resultado = operando1 - operando2;
+        } else if (operador === "*") {
+            resultado = operando1 * operando2;
+        } else if (operador === "/") {
+            if (operando2 == 0) {
+                alert("Não é possível dividir por zero")
+                continua = prompt(
+                    "Digite sim para continuar e não para encerrar")
+
+                if (continua === "não") {
+                return //return força a parada de uma função
+                } 
+            
+                
+
+            
+    
+            }
+        resultado = operando1 / operando2;
+        } else {
+            alert("Operador inválido!");
+            operador_valido = false; 
+            continua = prompt ("Digite sim para continuar e não para encerrar");
+            if (continua === "não") {
+
+                return //return força a parada de uma função
+
+            } 
+    
+        }
+
+        if ( (operador != "/" || operando2 != 0) && operador_valido != false ) {
+            alert("Resultado: " +
+                operando1 + 
+                " " + 
+                operador + 
+                " " + 
+                operando2 + 
+                " = " + 
+                resultado);
 
         }
-        resultado = operando1 / operando2;
-    } else {
-        alert("Operador inválido!");
-        return //return força a parada de uma função
+        // o símbolo || significa "ou" 
     }
-
-    alert("resultado: " + 
-        operando1 + " " + operador + " " + operando2 + " = " + resultado);
-    }
-
-}
+} 
